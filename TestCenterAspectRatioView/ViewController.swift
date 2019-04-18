f964445724b2f33883e571352ccea55fd47b83fe
Bart van Kuik <bart@vankuik.nl>
2019-04-18 19:46:19 +0200

    Initial Commit

---
+++ TestCenterAspectRatioView/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TestCenterAspectRatioView
+//
+//  Created by Bart van Kuik on 18/04/2019.
+//  Copyright © 2019 Example. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
